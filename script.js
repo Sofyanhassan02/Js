0a2d64c46a2b897abfbeef611b9d6b35f5d0fab9
Sofyanhassan02 <sofyanhassan60@gmail.com>
2024-03-01 10:22:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// Part 1
+
+
+// 1-
+// let array = [1, 2, 3, 4, 5];
+// let sum = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+
+// console.log("The sum of the numbers is:", sum);
+
+
+
+
+// 2-
+// let str = "    Hello world!   ";
+// let stringWithoutSpaces = str.trim();
+// console.log("String without spaces:",stringWithoutSpaces);
+
+
+
+
+// 3-
+// function isDivisibleBy10(number) {
+//     return number % 10 === 0;
+// }
+// let numFromUser=parseInt(prompt("enter number"))
+// console.log(isDivisibleBy10(numFromUser)); 
+
+
+
+
+
+// 4-
+// function maxNumber(a, b) {
+//     return a > b ? a : b;
+// }
+// let num1 =parseInt(prompt("enter num 1"));
+// let num2 =parseInt(prompt("enter num 2"));
+// console.log(maxNumber(num1, num2));
+
+
+
+
+// part2
+
+// 1-String 
+
+// 2-concatenation
+
+// 3-Logical AND 
